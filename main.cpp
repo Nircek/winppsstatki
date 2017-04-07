c0edae641b947f872a314211c7d581b221917c09
Nircek <marcinek0302@gmail.com>
2017-04-07 16:47:26 +0200

    Add basic login menu

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include "winppspc/winppspc.h"
 
 using namespace std;
-
+using namespace GitHub::Nircek;
 int main()
 {
+    string host,user,pass;
+    PPSReply reply(0,"");
+    winppspc *pc=(winppspc*)false;
+    do{
+        if(pc){
+            delete pc;
+            cout<<"cathed "<<readPPSReplyType(reply.replyType)<<' '<<reply.reply<<'\n';
+        }
+        cout<<"PPS host: ";cin>>host;
+        cout<<"Username: ";cin>>user;
+        cout<<"Password: ";cin>>pass;
+        cin.ignore();
+        pc=new winppspc(host,user,pass);
+    }while((reply=pc->push("on")).replyType!=good);
+
     cout << "Hello world!" << endl;
     return 0;
 }
