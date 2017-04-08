12d52b1c5f5f98003beddaca7135207444b2863c
Nircek <marcinek0302@gmail.com>
2017-04-08 16:02:33 +0200

    Add register handling

--- main.cpp
+++ main.cpp
@@ -5,21 +5,31 @@ using namespace std;
 using namespace GitHub::Nircek;
 int main()
 {
-    string host,user,pass;
-    PPSReply reply(0,"");
+    string action,host,user,pass;
+    bool cont;
     winppspc *pc=(winppspc*)false;
     do{
+        cont=false;
         if(pc){
             delete pc;
-            cout<<"cathed "<<readPPSReplyType(reply.replyType)<<' '<<reply.reply<<'\n';
         }
-        cout<<"PPS host: ";cin>>host;
+        cout<<"\nPPS host: ";cin>>host;
+        cout<<"1)Login\n2)Register\n";
+        do{
+            cin>>action;
+        }while((action!="1")&&(action!="2"));
         cout<<"Username: ";cin>>user;
         cout<<"Password: ";cin>>pass;
         cin.ignore();
-        pc=new winppspc(host,user,pass);
-    }while((reply=pc->push("on")).replyType!=good);
+        if(action=="1"){
+            pc=new winppspc(host,user,pass);
+        }else{
+            pc=new winppspc(host);
+            if(readerror(pc->uregister(user,pass)).replyType!=good)cont=true;
+        }
+    }while(cont?true:readerror(pc->push("on")).replyType!=good);
 
     cout << "Hello world!" << endl;
+    delete pc;
     return 0;
 }
